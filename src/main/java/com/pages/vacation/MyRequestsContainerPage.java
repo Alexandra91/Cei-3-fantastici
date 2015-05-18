ca7eadc2c13dda1e3a9c22ca9ca2627b8afc0dfe
alexandrapandele <alexandrapandele@EVO235.evointern>
2015-05-18 16:24:42 +0300

    added file renamed

---
+++ src/main/java/com/pages/vacation/MyRequestsContainerPage.java
@@ -0,0 +1,27 @@
+package com.pages.vacation;
+import java.util.List;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.PageObject;
+
+public class MyRequestsContainerPage extends PageObject{
+	@FindBy(css="td[class*='col-4']") 
+	List<WebElement> coloanaType;
+
+	public void checkVacationType(String vactype){
+		boolean check=false;
+		for(WebElement element: coloanaType){
+		if (!element.getText().contentEquals(vactype)) {
+			check=true;
+		}	
+			Assert.assertFalse("Element was found",check);
+		}
+			
+	}
+
+
+
+}
